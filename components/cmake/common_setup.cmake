bcfe70b9ac26cd2df130ec21569aa0680d022acf
Benjamin R. Hillman <bhillma@sandia.gov>
2020-08-13 19:57:42 -0400

    Add option -rrtmgpxx to configure
    Add option -rrtmgpxx to configure to build the RRTMGP++ code instead of RRTMGP.

--- components/cmake/common_setup.cmake
+++ components/cmake/common_setup.cmake
@@ -380,6 +380,12 @@ if (NOT HAS_SAMXX EQUAL -1)
   set(USE_SAMXX TRUE)
 endif()
 
+string(FIND "${CAM_CONFIG_OPTS}" "-rrtmgpxx" HAS_RRTMGPXX)
+if (NOT HAS_RRTMGPXX EQUAL -1)
+  # The following is for the RRTMGPXX code:
+  set(USE_RRTMGPXX TRUE)
+endif()
+
 # System libraries (netcdf, mpi, pnetcdf, esmf, trilinos, etc.)
 if (NOT SLIBS)
   if (NOT NETCDF_SEPARATE)
