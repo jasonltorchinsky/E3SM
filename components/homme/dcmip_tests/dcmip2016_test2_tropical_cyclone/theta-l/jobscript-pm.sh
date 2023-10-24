c91fcec1c13eb44faa5983193f92a43ebfff3a84
Jason L Torchinsky <jltorchinsky@gmail.com>
2023-10-24 06:36:57 -0700

    Adds Perlmutter-relevant scripts, adjusts machine files and CMakeLists for DCMIP Test case compatibility on Perlmutter.

--- components/homme/dcmip_tests/dcmip2016_test2_tropical_cyclone/theta-l/jobscript-pm.sh
+++ components/homme/dcmip_tests/dcmip2016_test2_tropical_cyclone/theta-l/jobscript-pm.sh
@@ -1,26 +1,26 @@
 #!/bin/bash 
 #
-#   Jobscript for launching dcmip2016 test2 on Cori KNL
+#   Jobscript for launching dcmip2016 test2 on Perlmutter
 #
 #SBATCH --job-name=dcmip-2016-2
 #SBATCH --output=%j.out
 #SBATCH --error=%j.err
 
 #SBATCH --qos=debug
-#SBATCH --nodes=32
-#SBATCH --constraint=knl
+#SBATCH --nodes=8
+#SBATCH --constraint=cpu
 
 #SBATCH --account=e3sm
-#SBATCH --mail-user=jason.torchinsky@wisc.edu
+#SBATCH --mail-user=jltorchinsky@gmail.com
 #SBATCH --mail-type=all
 
 #SBATCH --time=0-00:30:00
 
 # Set variables, load modules
 EXEC=../../../test_execs/theta-l-nlev30/theta-l-nlev30
-module load python
-conda activate geocat
-module load ncl
+# module load python
+# conda activate geocat
+module load climate-utils/2023q2
 
 function run {
 namelist=namelist-$prefix.nl
@@ -31,11 +31,11 @@ date
 ncl plot-tropical-cyclone-init.ncl  # u,t,th,q,pnh,geo,ps, time=0
 ncl plot-horiz-crossx.ncl     # contour plot, time=10d, U,V,T,ps,precl,Q,geo
 #ncl plot-intensity-trace.ncl
-python plot-ps.py
-python plot-intensity.py
-python plot-precip.py
-python plot-Ts.py
-python plot-uvs.py
+#python plot-ps.py
+#python plot-intensity.py
+#python plot-precip.py
+#python plot-Ts.py
+#python plot-uvs.py
 #ncl plot-horiz-ps.ncl
 
 # save output
@@ -45,11 +45,11 @@ python plot-uvs.py
 \mv -f x-sections.pdf ${prefix}_x-sections.pdf
 #\mv -f wind.pdf ${prefix}_wind.pdf
 #\mv -f psmap.pdf ${prefix}_psmap.pdf
-\mv -f ps.pdf ${prefix}_ps.pdf
-\mv -f intensity.pdf ${prefix}_intensity.pdf
-\mv -f precip.pdf ${prefix}_precip.pdf
-\mv -f Ts.pdf ${prefix}_Ts.pdf
-\mv -f uvs.pdf ${prefix}_uvs.pdf
+#\mv -f ps.pdf ${prefix}_ps.pdf
+#\mv -f intensity.pdf ${prefix}_intensity.pdf
+#\mv -f precip.pdf ${prefix}_precip.pdf
+#\mv -f Ts.pdf ${prefix}_Ts.pdf
+#\mv -f uvs.pdf ${prefix}_uvs.pdf
 }
 
 prefix=r100;  run
